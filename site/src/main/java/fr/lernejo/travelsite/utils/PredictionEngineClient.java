20071bcc8700e0e212cc322a5a3d3f40d069a731
EdMkn <nmanyim@rocketmail.com>
2022-01-08 11:24:28 +0100

    Partie 4 Création de la classe Prédiction Engine et modification Launcher

--- site/src/main/java/fr/lernejo/travelsite/utils/PredictionEngineClient.java
+++ site/src/main/java/fr/lernejo/travelsite/utils/PredictionEngineClient.java
@@ -1,2 +1,8 @@
-package fr.lernejo.travelsite.utils;public class PredictionEngineClient {
+package fr.lernejo.travelsite.utils;
+
+import org.springframework.context.annotation.Bean;
+import retrofit2.Retrofit;
+
+public interface PredictionEngineClient {
+
 }
